02ad3bf4214e4941f4474465c1849a0fe9e0fcc5
Nate Deisler <ineanderthals@Nates-MacBook-Pro.local>
2019-03-07 12:45:16 -0500

    login component and servie

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { HttpClientModule } from "@angular/common/http";
+import { GardenService } from "./garden.service";
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavComponent } from './nav/nav.component';
 import { AboutComponent } from './about/about.component';
 import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     AboutComponent,
-    ProductsComponent
+    ProductsComponent,
+    HttpClientModule,
+    LoginComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule
   ],
-  providers: [],
+  providers: [GardenService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
